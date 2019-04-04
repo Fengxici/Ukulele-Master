929e8ff10248c26e2927a8afeb92ebd1ccf45b95
910980941@qq.com <910980941@qq.com>
2019-04-04 08:08:18 +0800

    redisson整合spring boot

--- ukulele-redisson/src/main/java/timing/ukulele/redisson/RedissonHelper.java
+++ ukulele-redisson/src/main/java/timing/ukulele/redisson/RedissonHelper.java
@@ -25,7 +25,9 @@ public class RedissonHelper implements CacheManager, ApplicationContextAware {
         this.applicationContext = applicationContext;
     }
 
-    // 获取连接
+    /**
+     * 获取连接
+     */
     private RedissonClient getRedis() {
         if (redisTemplate == null) {
             synchronized (RedissonHelper.class) {
@@ -37,10 +39,22 @@ public class RedissonHelper implements CacheManager, ApplicationContextAware {
         return redisTemplate;
     }
 
+    /**
+     * 根据键值获取对象桶
+     *
+     * @param key 键值
+     * @return 对象桶
+     */
     private RBucket<Object> getRedisBucket(String key) {
         return getRedis().getBucket(key);
     }
 
+    /**
+     * 根据键值获取对象
+     *
+     * @param key 键值
+     * @return 对象
+     */
     public final Object get(final String key) {
         RBucket<Object> temp = getRedisBucket(key);
 //		get时暂时不设置过期时间 吕自聪 2017/06/15
@@ -48,35 +62,75 @@ public class RedissonHelper implements CacheManager, ApplicationContextAware {
         return temp.get();
     }
 
+    /**
+     * 设置某个键值，默认的过期时间
+     *
+     * @param key   键
+     * @param value 值
+     */
     public final void set(final String key, final Serializable value) {
         RBucket<Object> temp = getRedisBucket(key);
         temp.set(value);
         expire(temp, EXPIRE);
     }
 
+    /**
+     * 设置某个键值，并指定过期时间
+     *
+     * @param key     键
+     * @param value   值
+     * @param seconds 过期时间
+     */
     public final void set(final String key, final Serializable value, int seconds) {
         RBucket<Object> temp = getRedisBucket(key);
         temp.set(value);
         expire(temp, seconds);
     }
 
+    /**
+     * 批量设置键值
+     *
+     * @param temps 键值对map
+     */
     public final void multiSet(final Map<String, Object> temps) {
         getRedis().getBuckets().set(temps);
     }
 
+    /**
+     * 判断某个键值是否存在
+     *
+     * @param key 键值
+     * @return 是否存在
+     */
     public final Boolean exists(final String key) {
         RBucket<Object> temp = getRedisBucket(key);
         return temp.isExists();
     }
 
+    /**
+     * 同步删除某个键及对应的值
+     *
+     * @param key 键
+     */
     public final void del(final String key) {
         getRedis().getKeys().deleteAsync(key);
     }
 
+    /**
+     * 删除匹配表达式的所有键及对应的值
+     *
+     * @param pattern 表达式
+     */
     public final void delAll(final String pattern) {
         getRedis().getKeys().deleteByPattern(pattern);
     }
 
+    /**
+     * 获取某个键对应的值的类型
+     *
+     * @param key 键
+     * @return 类型字符串
+     */
     public final String type(final String key) {
         RType type = getRedis().getKeys().getType(key);
         if (type == null) {
@@ -98,7 +152,7 @@ public class RedissonHelper implements CacheManager, ApplicationContextAware {
     /**
      * 在某个时间点失效
      *
-     * @param key 键值
+     * @param key      键值
      * @param unixTime 时间
      * @return 是否设置时间
      */
@@ -119,8 +173,7 @@ public class RedissonHelper implements CacheManager, ApplicationContextAware {
     public Set<Object> getAll(String pattern) {
         Set<Object> set = new HashSet<>();
         Iterable<String> keys = getRedis().getKeys().getKeysByPattern(pattern);
-        for (Iterator<String> iterator = keys.iterator(); iterator.hasNext(); ) {
-            String key = iterator.next();
+        for (String key : keys) {
             set.add(getRedisBucket(key).get());
         }
         return set;
