ade065c5105bf390cc5f7202ad9d924a3be9c63c
fengxici <fengxici1@gmail.com>
2019-04-29 00:16:15 +0800

    feign及web的日期格式化

--- ukulele-ribbon/src/main/java/timing/ukulele/ribbon/UkuleleRibbonAutoConfigure.java
+++ ukulele-ribbon/src/main/java/timing/ukulele/ribbon/UkuleleRibbonAutoConfigure.java
@@ -1,5 +1,12 @@
 package timing.ukulele.ribbon;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.Registry;
@@ -10,30 +17,54 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.DefaultPropertiesFactory;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import timing.ukulele.ribbon.config.UkuleleRestTemplateProperties;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 /**
  * Ribbon扩展配置类
  */
 @Configuration
-@EnableWebMvc
 @EnableCircuitBreaker
 @EnableConfigurationProperties(UkuleleRestTemplateProperties.class)
-public class UkuleleRibbonAutoConfigure implements WebMvcConfigurer {
+public class UkuleleRibbonAutoConfigure extends FeignClientsConfiguration implements WebMvcConfigurer {
+    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private final UkuleleRestTemplateProperties restTemplateProperties;
+    @Autowired
+    public UkuleleRibbonAutoConfigure(UkuleleRestTemplateProperties restTemplateProperties) {
+        this.restTemplateProperties = restTemplateProperties;
+    }
 
     @Bean
     public DefaultPropertiesFactory defaultPropertiesFactory() {
@@ -44,9 +75,6 @@ public class UkuleleRibbonAutoConfigure implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new XlabelMvcHeaderInterceptor());
     }
-
-    @Autowired
-    private UkuleleRestTemplateProperties restTemplateProperties;
 
     @LoadBalanced
     @Bean
@@ -95,4 +123,131 @@ public class UkuleleRibbonAutoConfigure implements WebMvcConfigurer {
                 .setRetryHandler(new DefaultHttpRequestRetryHandler(3, true))
                 .build();
     }
+
+    /**
+     * 时间格式转换器,将Date类型统一转换为yyyy-MM-dd HH:mm:ss格式的字符串
+     * 长整型转换成String
+     * 忽略value为null时key的输出
+     */
+    private ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //序列化日期格式
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer());
+        javaTimeModule.addSerializer(Date.class, new DateSerializer(false, simpleDateFormat));
+        //反序列化日期格式
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer());
+        javaTimeModule.addDeserializer(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                String date = jsonParser.getText();
+                try {
+                    return simpleDateFormat.parse(date);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        objectMapper.registerModule(javaTimeModule);
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
+        return objectMapper;
+    }
+
+
+    /**
+     * LocalDateTime序列化
+     */
+    private class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+
+        @Override
+        public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(DATETIME_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalDateTime反序列化
+     */
+    private class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+        @Override
+        public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalDateTime.parse(p.getValueAsString(), DATETIME_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalDate序列化
+     */
+    private class LocalDateSerializer extends JsonSerializer<LocalDate> {
+
+        @Override
+        public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(DATE_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalDate反序列化
+     */
+    private class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+        @Override
+        public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalDate.parse(p.getValueAsString(), DATE_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalTime序列化
+     */
+    private class LocalTimeSerializer extends JsonSerializer<LocalTime> {
+
+        @Override
+        public void serialize(LocalTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(TIME_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalTime反序列化
+     */
+    private class LocalTimeDeserializer extends JsonDeserializer<LocalTime> {
+
+        @Override
+        public LocalTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalTime.parse(p.getValueAsString(), TIME_FORMATTER);
+        }
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(customObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
+
+    @Bean
+    public Encoder feignEncoder() {
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(customObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new SpringEncoder(objectFactory);
+    }
+
+    public ObjectMapper customObjectMapper() {
+        ObjectMapper objectMapper = serializingObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        return objectMapper;
+    }
+
 }
--- ukulele-web/src/main/java/timing/ukulele/web/config/WebDataConvertConfig.java
+++ ukulele-web/src/main/java/timing/ukulele/web/config/WebDataConvertConfig.java
@@ -1,24 +1,37 @@
 package timing.ukulele.web.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 @Configuration
 public class WebDataConvertConfig implements WebMvcConfigurer {
+
+    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
@@ -45,19 +58,120 @@ public class WebDataConvertConfig implements WebMvcConfigurer {
 //        mapper.registerModule(simpleModule);
 //        converter.setObjectMapper(mapper);
 //        return converter;
-
-        ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().createXmlMapper(false).build();
+//        ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().createXmlMapper(false).build();
+        ObjectMapper objectMapper = serializingObjectMapper();
         //忽略value为null时key的输出
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         //序列化成json时，将所有的Long变成string，以解决js中的精度丢失。
         SimpleModule simpleModule = new SimpleModule();
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        simpleModule.addSerializer(Date.class, new DateSerializer(false,simpleDateFormat));
         objectMapper.registerModule(simpleModule);
         converter.setObjectMapper(objectMapper);
         return converter;
+    }
 
+    /**
+     * jackson2 json序列化 null字段输出为空串
+     */
+    private ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //序列化日期格式
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer());
+        javaTimeModule.addSerializer(Date.class,new DateSerializer(false,simpleDateFormat));
+        //反序列化日期格式
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer());
+        javaTimeModule.addDeserializer(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                String date = jsonParser.getText();
+                try {
+                    return simpleDateFormat.parse(date);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        objectMapper.registerModule(javaTimeModule);
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
+        return objectMapper;
+    }
+
+
+    /**
+     * LocalDateTime序列化
+     */
+    private class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+
+        @Override
+        public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(DATETIME_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalDateTime反序列化
+     */
+    private class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+        @Override
+        public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalDateTime.parse(p.getValueAsString(), DATETIME_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalDate序列化
+     */
+    private class LocalDateSerializer extends JsonSerializer<LocalDate> {
+
+        @Override
+        public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(DATE_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalDate反序列化
+     */
+    private class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+        @Override
+        public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalDate.parse(p.getValueAsString(), DATE_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalTime序列化
+     */
+    private class LocalTimeSerializer extends JsonSerializer<LocalTime> {
+
+        @Override
+        public void serialize(LocalTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(TIME_FORMATTER));
+        }
+    }
+
+    /**
+     * LocalTime反序列化
+     */
+    private class LocalTimeDeserializer extends JsonDeserializer<LocalTime> {
+
+        @Override
+        public LocalTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalTime.parse(p.getValueAsString(), TIME_FORMATTER);
+        }
     }
 }
