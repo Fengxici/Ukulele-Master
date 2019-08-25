5350e9f695d095a4ec8ac353749502ddc5cb088e
910980941@qq.com <910980941@qq.com>
2019-08-25 11:48:46 +0800

    pom信息完善，单利模式优化

--- ukulele-http/src/main/java/timing/ukulele/http/OkHttpManager.java
+++ ukulele-http/src/main/java/timing/ukulele/http/OkHttpManager.java
@@ -3,101 +3,92 @@ package timing.ukulele.http;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
  * okHttp3 管理类，封装了okHttp的网络连接方法
- * OkHttp3 开启Stetho听诊器
  */
-public class OkHttpManager {
-    protected static OkHttpManager instance;
+public enum OkHttpManager {
+    INSTANCE;
 
-    public static OkHttpManager getInstance() {
-        if (instance == null) {
-            synchronized (OkHttpManager.class) {
-                if (instance == null) {
-                    instance = new OkHttpManager();
-                }
-            }
-        }
-        return instance;
+    public OkHttpManager getInstance() {
+        return INSTANCE;
     }
 
-    protected static OkHttpClient okHttpClient;
-    protected static OkHttpClient.Builder builder;
-    protected int mTimeOut=60;
-    protected String headerKey,headerValue;
-    protected boolean isLog=true;
-
-    public OkHttpManager() {
-    }
+    protected OkHttpClient okHttpClient;
+    protected OkHttpClient.Builder builder;
+    protected int mTimeOut = 60;
+    protected String headerKey, headerValue;
+    protected boolean isLog = true;
 
     /**
      * 设置header
      * 暂时不支持多个header
-     * @param headerKey
-     * @param headerValue
+     *
+     * @param headerKey   键
+     * @param headerValue 值
      * @return
      */
-    public OkHttpManager setHeader(String headerKey,String headerValue) {
+    public OkHttpManager setHeader(String headerKey, String headerValue) {
         this.headerKey = headerKey;
         this.headerValue = headerValue;
-        return instance;
+        return INSTANCE;
     }
 
     /**
      * 设置超时时间
+     *
      * @param timeOut 数值
      * @return
      */
     public OkHttpManager setTimeOut(int timeOut) {
         mTimeOut = timeOut;
-        return instance;
+        return INSTANCE;
     }
 
     /**
      * 是否显示Log 默认为true
+     *
      * @param isLog true 显示
      * @return
      */
-    public OkHttpManager setLog(boolean isLog){
-        this.isLog=isLog;
-        return instance;
+    public OkHttpManager setLog(boolean isLog) {
+        this.isLog = isLog;
+        return INSTANCE;
     }
 
-    public OkHttpManager build(List<Interceptor> interceptors){
+    public OkHttpManager build(List<Interceptor> interceptors) {
         if (okHttpClient == null) {
             builder = new OkHttpClient.Builder()
                     .addInterceptor(new HttpLoggingInterceptor()
-                            .setLevel(isLog? HttpLoggingInterceptor.Level.BODY: HttpLoggingInterceptor.Level.NONE))
+                            .setLevel(isLog ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE))
                     .retryOnConnectionFailure(true)
                     .connectTimeout(mTimeOut, TimeUnit.SECONDS)
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request response = chain.request();
-                            if (headerKey != null) {
-                                response = chain.request()
+                    .readTimeout(mTimeOut, TimeUnit.SECONDS)
+                    .writeTimeout(mTimeOut, TimeUnit.SECONDS)
+                    .addInterceptor(chain -> {
+                        Request response = chain.request();
+                        if (headerKey != null) {
+                            response = chain.request()
                                     .newBuilder()
                                     .addHeader(headerKey, headerValue)
                                     .build();
-                            }
-                            return chain.proceed(response);
                         }
+                        return chain.proceed(response);
                     });
-            if(interceptors!=null){
-                for(Interceptor interceptor:interceptors){
-                    builder.addInterceptor(interceptor);
-                }
-            }
+
             okHttpClient = builder.build();
         }
-        return instance;
+        builder.interceptors().clear();
+        if (interceptors != null) {
+            for (Interceptor interceptor : interceptors) {
+                builder.addInterceptor(interceptor);
+            }
+        }
+        return INSTANCE;
     }
 
     /**
@@ -107,7 +98,7 @@ public class OkHttpManager {
         return okHttpClient;
     }
 
-    public OkHttpClient.Builder getBuilder(){
+    public OkHttpClient.Builder getBuilder() {
         return builder;
     }
 }
