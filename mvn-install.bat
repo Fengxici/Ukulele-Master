a4854e6d540fb54ecf092e7ef9ffa86917526e3b
910980941@qq.com <910980941@qq.com>
2019-12-14 23:55:59 +0800

    相关脚本

---
+++ mvn-install.bat
@@ -0,0 +1,5 @@
+echo off
+echo install work is running.....
+title web
+cmd /k mvn clean install
+echo install work is done
---
+++ mvn-package.bat
@@ -0,0 +1,5 @@
+echo off
+echo package work is running.....
+title web
+cmd /k mvn clean package
+echo package work is done
