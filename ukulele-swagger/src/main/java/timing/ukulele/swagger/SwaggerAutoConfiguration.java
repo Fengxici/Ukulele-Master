dc099267d12297d5c1cd0c65dfeacdade970dc57
910980941@qq.com <910980941@qq.com>
2020-03-06 14:56:46 +0800

    配置勘误

--- ukulele-swagger/src/main/java/timing/ukulele/swagger/SwaggerAutoConfiguration.java
+++ ukulele-swagger/src/main/java/timing/ukulele/swagger/SwaggerAutoConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -23,6 +24,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Configuration
+@EnableConfigurationProperties(SwaggerProperties.class)
 @Import({Swagger2Configuration.class})
 public class SwaggerAutoConfiguration implements BeanFactoryAware {
 
